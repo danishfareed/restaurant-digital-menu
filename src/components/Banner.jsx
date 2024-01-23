a39276fb539cf15ca60dda506472abce25c21874
Mohammed Danish <danish.m@cheildubai.local>
2024-01-23 16:56:12 +0400

    updated menu banner UI

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,39 @@ import React from 'react';
 
 function Banner({ backgroundImage, title }) {
   return (
-    <div className="relative text-white text-center py-24" style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }}>
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-      <h1 className="text-4xl font-bold relative z-10">{title}</h1>
+    <>
+ <div className="relative text-black text-center">
+  <img
+    src={backgroundImage}
+    alt="Background"
+    className="object-cover w-full h-64"
+  />
+  
+
+  <div className="relative inset-0 flex items-center justify-center overflow px-6 -mt-12 pb-6">
+    <div className="bg-white border border-gray-300 rounded-md p-4 max-w-md w-full flex items-center">
+      {/* Logo on the left (25%) */}
+      <div className="w-1/4 border border-gray-300 rounded-md flex items-center justify-center flex-col">
+        <img
+          src="https://tgifridaysme.com/wp-content/themes/tgifridays-theme/assets/images/logo-tgifridays.png"
+          alt="TGI Fridays Logo"
+          className="max-w-full h-auto m-4"
+        />
+      </div>
+
+
+
+      {/* Main Title and Subtitle on the right (75%) */}
+      <div className="w-3/4 text-left pl-8">
+        <h1 className="text-2xl font-bold mb-1">TGI Fridays</h1>
+        <p className="text-lg">{title}</p>
+      </div>
     </div>
+  </div>
+</div>
+
+
+    </>
   );
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,27 @@ import React from 'react'
 
 export const HomePage = () => {
   return (
-    <div>This is homepage</div>
+    <>
+    <div className="max-w-xl mx-auto bg-white p-8 rounded shadow-md">
+      <img
+        src="https://tgifridaysme.com/wp-content/themes/tgifridays-theme/assets/images/logo-tgifridays.png"
+        alt="TGI Fridays Logo"
+        className="max-w-full h-auto mb-4"
+      />
+
+      <a
+        href="https://www.tgifridays.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline mb-4 block"
+      >
+        Visit TGI Fridays Website
+      </a>
+
+      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        Click Me
+      </button>
+    </div>
+    </>
   )
 }
